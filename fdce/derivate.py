13e4f4f531387d465382065e839e3ffbf0cbd60e
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-06-07 13:27:20 -0400

    Add derivation src function docstring

--- fdce/derivate.py
+++ fdce/derivate.py
@@ -6,6 +6,25 @@ from fdce.get_coeff import get_coeff
 def derivate(
     x: np.ndarray, y: np.ndarray, order: int = 1, accuracy: int = 1
 ) -> np.ndarray:
+    """
+    Calculate the derivate of a function.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        The x-values of the function.
+    y : np.ndarray
+        The y-values of the function.
+    order : int
+        The order of the derivate.
+    accuracy : int
+        The accuracy of the derivate.
+
+    Returns
+    -------
+    np.ndarray
+        The derivate of the function.
+    """
     result = np.empty((x.shape[0] - accuracy,))
     a_len = accuracy + 1
     coeff_arr = np.empty((order + 1, a_len, a_len))
